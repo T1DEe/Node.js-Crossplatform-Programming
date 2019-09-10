53818bd72d58f1ed4641cca8ed662cda5a5f8140
Artemy Markovsky <artemexe2013@gmail.com>
2019-09-10 14:00:35 +0300

    Lab03

---
+++ Lab03/03-01.js
@@ -0,0 +1,36 @@
+const http = require('http');
+var readline = require('readline');
+
+const server = http.createServer(function (request, response) {
+    response.contentType='text/html';
+    response.write('<h1>norm</h1>');
+
+    process.stdin.setEncoding('utf-8');
+    process.stdin.on('readable', ()=>{
+
+        let chunc = 'norm';
+        while((chunc = process.stdin.read()) != null ) {
+            if (chunc.trim() == 'exit') 
+                process.exit(0);
+            else if (chunc.trim() == 'norm') {
+                process.stdout.write('State changed to – stop \n');
+                response.write('<h1>stop</h1>');
+            }
+            else if (chunc.trim() == 'stop') {
+                process.stdout.write('State changed to – stop \n');
+                response.write('<h1>stop</h1>');
+            }
+            else if (chunc.trim() == 'test') {
+                process.stdout.write('State changed to – test \n');
+                response.write('<h1>test</h1>');
+            }
+            else if (chunc.trim() == 'idle') {
+                process.stdout.write('State changed to – idle \n');
+                response.write('<h1>idle</h1>');
+            }
+            else process.stdout.write(chunc)
+        }
+    }); 
+}).listen(5000);
+
+
