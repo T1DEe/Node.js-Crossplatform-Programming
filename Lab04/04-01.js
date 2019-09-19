e6f79ccee80c9075ebbb9a3bf1971c63ab328bc0
Artemy Markovsky <artemexe2013@gmail.com>
2019-09-19 23:47:29 +0300

    Lab04 finish

--- Lab04/04-01.js
+++ Lab04/04-01.js
@@ -1,17 +1,50 @@
 const http = require('http');
-let DB = require('DB');
+const url = require('url');
+const fs = require('fs');
+const DB = require('./DB');
+
+let db = new DB();
+
+//===== слушатели =======================================
+db.on('GET', (request, response) => {
+    response.end(JSON.stringify(db.getAllRows()));
+});
+
+db.on('POST', (request, response) => {
+    request.on('data', data => {
+        let row = JSON.parse(data);
+        row.id = db.lastIndex() + 1;
+        db.addRow(row);
+        response.end(JSON.stringify(row));
+    });
+});
+
+db.on('PUT', (request) => {
+    request.on('data', data => {
+        let row = JSON.parse(data);
+        db.editRow(row.id, row)
+    });
+});
+
+db.on('DELETE', (request, response) => {
+    if (typeof url.parse(request.url, true).query.id != 'undefined') {
+        let id = parseInt(url.parse(request.url, true).query.id);
+        if (Number.isInteger(id)) {
+            let deletedRow = db.deleteRow(id);
+
+            response.writeHead(200, {'Content-Type' : 'application/json'});
+            response.end(JSON.stringify(deletedRow));
+        }
+    }
+});
+//=======================================================
 
 http.createServer('request', (request, response) => {
-    if (request.url === '/api/db') {
-        switch(request.method) {
-            case 'GET': 
-
-            case 'POST':
-
-            case 'PUT':
-            
-            case 'DELETE':
-                
-        }
+    if (request.url === '/') {
+        let html = fs.readFileSync('./04-02.html');
+        response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        response.end(html);
+    } else if (url.parse(request.url).pathname === '/api/db') {
+        db.emit(request.method, request, response); 
     }
 }).listen(5000)
--- Lab04/DB.js
+++ Lab04/DB.js
@@ -1,25 +1,48 @@
-class DB {
-    array = [
-        { id: 1, name : "a", bday : "01.01.2001" },
-        { id: 2, name : "b", bday : "02.02.2002" },
-        { id: 3, name : "c", bday : "03.03.2003" },
-        { id: 4, name : "d", bday : "04.04.2004" }];
+const util = require('util');
+const events = require('events');
 
-    selectAllRows() {
+dbData = [
+    { id: 0, name : 'aaaaaaaa', bday : '01.01.2001' },
+    { id: 1, name : 'bbbbbbbb', bday : '02.02.2002' },
+    { id: 2, name : 'cccccccc', bday : '03.03.2003' },
+    { id: 3, name : 'dddddddd', bday : '04.04.2004' }
+];
 
+    function DB() {
+        //GET request
+        this.getAllRows = () => {
+            return dbData;
+        };
+        //POST request
+        this.addRow = (row) => {
+            dbData.push(row);
+        };
+        //PUT request
+        this.editRow = (id, row) => {
+            let oldValue = dbData.find(element => element.id == id);
+            let index = dbData.indexOf(oldValue)
+            
+            if (index !== -1) {
+                dbData[index] = row;
+            }
+        };
+        //DELETE request
+        this.deleteRow = (id) => {
+            let row;
+            for(let i = 0; i < dbData.length; i++) { 
+                if (dbData[i].id == id) {
+                    row = dbData[i];
+                    dbData.splice(i, 1);
+                }
+            }
+            return row;
+        };
+
+        this.lastIndex = () => {
+            return dbData.length - 1;
+        }
     }
 
-    addRow() {
 
-    }
-
-    editRow() {
-
-    }
-
-    deleteRow() {
-
-    }
-}
-
+util.inherits(DB, events.EventEmitter);
 module.exports = DB;
